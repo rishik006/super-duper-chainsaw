fc6c9eabe65ada89f5034df4fa5cf7ccf4d72a49
rishik006 <97524363+rishik006@users.noreply.github.com>
2023-05-30 17:29:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img=""
+status = ""
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640,420)
+    canvas.center()
+    onbjectdetecor = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status:Detecting objects";
+    document.getElementById("h5").innerHTML = "There are 3 objects in this room from which 2 have been identified"
+}
+
+
+function draw(){
+    image(img,0,0,640,420)
+    fill("#FF0000")
+    text("Dog",45,75).style
+    noFill()
+    stroke("#FF0000")
+    rect(30,60,450,350)
+
+    fill("#FF0000")
+    text("Cat",320,120)
+    noFill()
+    stroke("#FF0000")
+    rect(300,90,270,320)
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true
+    onbjectdetecor.detect(img, gotResults)
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+}
